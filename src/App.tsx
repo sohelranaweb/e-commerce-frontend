c477b917b78c4d52412ad88d940a5b78d48fed8f
sohelranaweb <sohel.rana.web2@gmail.com>
2025-09-05 12:32:15 +0600

    project set up

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to E-commerce</h1>
+        <div>
+          <Button>Click me</Button>
+          <Button>Hit me</Button>
+          <Button>Punch me</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
